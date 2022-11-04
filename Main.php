19e77f604f37ce622cdd41f7f71b54ab066574f4
Harsh_324 <harshravi324@gmail.com>
2022-11-04 20:52:29 +0530

    Main.php file is updated

---
+++ Main.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylehseet" type="text/css" href="style.css">
+    <title>Main</title>
+</head>
+<body>
+    <div id="Up_div">
+
+    </div>
+
+    <div id="Down_div">
+
+        <div id="Left_div">
+
+        </div>
+
+        <div id="Mid_div">
+
+        </div>
+
+        <div id="Right_div">
+
+        </div>
+
+    </div>
+
+</body>
+</html>
